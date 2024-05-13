9c5669442ce2fc4066be15d1521d72d925d600b7
esther2000 <estherploeger@hotmail.com>
2024-05-13 09:20:40 +0200

    add multi-value binarization option

--- data/compute_all_distances.py
+++ data/compute_all_distances.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-Description:    ...
-Usage:          ...
+Description:    Calculate distances between pairs of languages based on Grambank feature values
+Usage:          python compute_all_distances.py -g <GRAMBANK_FOLDER> -o <OUTPUT_FILE> -b (optional)
+
+TODO: add weighted similarity: rare features, transfer learning relevance
 """
+
 
 import argparse
 import pandas as pd
@@ -11,21 +14,45 @@ from sklearn.metrics.pairwise import nan_euclidean_distances
 
 def create_arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--gb_folder", type=str,
+    parser.add_argument("-g", "--gb_folder", type=str,
                         default='sources/grambank-v1.0.3',
                         help="Folder where Grambank download is stored")
+    parser.add_argument("-o", "--output_file", type=str,
+                        default='gb_vec_sim.csv',
+                        help="Name of file that output distances should be written to")
+    parser.add_argument("-b", "--binarize", action='store_true',
+                        help="Option for binarizing multi-value features")
     args = parser.parse_args()
     return args
+
+
+def binarize(gb_df, mv_feats):
+    """ Binarize multi-value features """
+    for feat in mv_feats.keys():
+        for val in range(0, mv_feats[feat]):
+            # binarize multi-value features
+            gb_df[f"{feat}_{str(val)}"] = (gb_df[f"{feat}"] == str(val)).astype(int).astype(str)
+            # if the original value was "?", put it back
+            gb_df.loc[gb_df[f"{feat}"] == "?", f"{feat}_{str(val)}"] = "?"
+
+    # remove original multi-value feature columns
+    gb_df = gb_df.drop(columns=mv_feats.keys())
+
+    return gb_df, gb_df.columns.to_list()[2:]
 
 
 def main():
 
     args = create_arg_parser()
-    # TODO: add weighted similarity: rare features, transfer learning relevance
+    mv_feats = {'GB024': 3, 'GB025': 3, 'GB065': 3, 'GB130': 3, 'GB193': 4, 'GB203': 4}  # IDs and num possible values
 
     # Load Grambank matrix
     gb_matrix = pd.read_csv('gb_lang_feat_vals.csv')
     gb_feats = [x for x in gb_matrix.columns.to_list() if x.startswith('GB')]
+
+    # Optional: binarize multi-value features
+    if args.binarize:
+        gb_matrix, gb_feats = binarize(gb_matrix, mv_feats)
 
     # Make vector per language
     lang_vecs = {row["Lang_ID"]: row[[x for x in gb_feats]].to_list() for _, row in gb_matrix.iterrows()}
@@ -39,7 +66,7 @@ def main():
 
     # Write to file
     sim_df = pd.DataFrame(sim_matrix, columns=langs, index=langs).fillna(0)
-    sim_df.to_csv('gb_vec_sim_0.csv')
+    sim_df.to_csv(args.output_file)
 
 
 if __name__ == '__main__':
