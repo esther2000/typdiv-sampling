ed837a11aa504b7e64b8b0d1c181aa0a7ac0c048
Wessel Poelman <contact+github@wesselpoelman.nl>
2024-06-13 09:59:58 +0200

    add k=20 results

--- mode-eval.sh
+++ mode-eval.sh
@@ -5,13 +5,13 @@ set -xe
 pip install -e .
 python data/compute_all_distances.py -b -n -g data/gb_lang_feat_vals.csv -o data/bin-norm-distances.csv
 python evaluation/experiment.py \
-    --results_path evaluation/experiments/only-k-11-eval.csv \
+    --results_path evaluation/experiments/only-k-20-eval.csv \
     --dist_path data/bin-norm-distances.csv \
     -gb_path grambank/cldf/languages.csv \
     -wals_path data/wals_dedup.csv \
     -gb_features_path data/gb_binarized.csv \
     -counts_path data/convenience_counts.json \
     -rand_runs 10 \
-    -s 11 \
-    -e 11 \
+    -s 20 \
+    -e 20 \
     -st 1
