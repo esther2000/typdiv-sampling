0ce692047679931ac02cef242bfd169e47dfdc7b
Wessel Poelman <contact+github@wesselpoelman.nl>
2024-07-04 15:41:12 +0200

    update data prep script

--- prepare-data.sh
+++ prepare-data.sh
@@ -4,11 +4,18 @@ set -xe
 
 # Grambank: download, pre-process, store default same as args
 # assume gb_lang_feat_vals.csv is here already, maybe pull from github and create? --> there actually is a separate python script for this
-python data/compute_all_distances.py -b -n -g data/gb_lang_feat_vals.csv -o data/bin-norm-distances.csv
+# assume wals is here already as well
 
-# WALS: download, pre-process, store default
+cd data
 
+# Glottolog
+wget https://cdstar.eva.mpg.de//bitstreams/EAEA0-CFBC-1A89-0C8C-0/glottolog_languoid.csv.zip
+unzip glottolog_languoid.csv.zip
+rm README.txt glottolog_languoid.csv.zip
 
-# TODO:
-# - add glottolog --> languoid.csv in DATA folder: https://cdstar.eva.mpg.de//bitstreams/EAEA0-CFBC-1A89-0C8C-0/glottolog_languoid.csv.zip
-# - add annotations for convenience baseline: https://github.com/WPoelman/typ-div-survey/blob/master/data/annotations-enhanced.csv
+# Annotations for convenience baseline
+wget https://raw.githubusercontent.com/WPoelman/typ-div-survey/master/data/annotations-enhanced.csv
+
+cd ..
+
+python data/compute_all_distances.py -b -n -c 0.25 -r -g data/gb_lang_feat_vals.csv -o data/bin-norm-distances.csv
